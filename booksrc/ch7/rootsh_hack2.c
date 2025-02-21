ed12458aa0f35dd8afde9302ae576e47819f75aa
tjmccoy <tyjmccoy@outlook.com>
2025-02-21 12:34:34 -0600

    Add M5 directory & changes to ~/ch7/rootsh_hack2

--- booksrc/ch7/rootsh_hack2.c
+++ booksrc/ch7/rootsh_hack2.c
@@ -11,6 +11,14 @@
  * Brief Description:
  * For details, please refer the book, Ch 7.
  */
+
+/*
+Order of execution is important here b/c changing the owner resets the execute bit to 'x'.
+After running 'make', run the following two commands: 
+	1) sudo chown root ./rootsh_hack2
+	2) sudo chmod u+s rootsh_hack2
+*/
+
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
@@ -19,14 +27,31 @@
 #include "../common.h"
 
 int main(int argc, char **argv)
-{
-	/* Become root */
-	if (setuid(0) == -1)
-		WARN("setuid(0) failed!\n");
+{	
+	// query real, effective, and saved-setuid
+	uid_t ruid, euid, suid;
+	getresuid(&ruid, &euid, &suid);
+	printf("ruid=%u, euid=%u, suid=%u\n", ruid, euid, suid);
 
-	/* Now just spawn a shell;
-	 * <i>Evil Laugh</i>, we're now root!
-	 */
+	if (argc > 1) {
+		/* Become root */
+		if (setuid(0) == -1) {
+			perror("setuid(0) failed!\n");
+			//WARN("setuid(0) failed!\n");
+			exit(EXIT_FAILURE);
+		}
+		getresuid(&ruid, &euid, &suid);
+		printf("ruid=%u, euid=%u, suid=%u\n", ruid, euid, suid);
+		setuid(1000);
+		getresuid(&ruid, &euid, &suid);
+		printf("ruid=%u, euid=%u, suid=%u\n", ruid, euid, suid);
+	}
+
+	/* 
+	Now just spawn a shell; 
+	<i>Evil Laugh</i>, we're now root! 
+	*/
+	 
 	system("/bin/bash");
 	exit (EXIT_SUCCESS);
 }
